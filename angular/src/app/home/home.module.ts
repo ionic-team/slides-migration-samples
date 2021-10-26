63dcb83993f51c9a6558eac03f611491aa19a228
Liam DeBeasi <liamdebeasi@icloud.com>
2021-10-26 12:47:12 -0400

    migrate(angular): import swiper module

--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -5,14 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    SwiperModule
   ],
   declarations: [HomePage]
 })
